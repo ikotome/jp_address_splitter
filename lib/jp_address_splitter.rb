edde3919c465ad4197c10230d5b0d742f2a03112
ikotome <kotomizu.0728@gmail.com>
2025-06-27 21:18:45 +0900

    とりあえず、full_addressを出力できるようになった

---
+++ lib/jp_address_splitter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jp_address_splitter/version"
+
+module JpAddressSplitter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jp_address_splitter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JpAddressSplitter
+  VERSION = "0.1.0"
+end
